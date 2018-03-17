117e8ee0f326186a2b9dd3fca4620ac97238459d
Vi Nguyen <fluffywin@gmail.com>
2018-03-16 20:37:27 -0400

    First Day

---
+++ Level 1/Assets/Control.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+    public float moveForce = 365.0f;
+    public float maxSpeed = 5.0f;
+    public float jumpForce = 1000.0f;
+
+
+    private Rigidbody2D player;
+
+
+	// Use this for initialization
+	void Start () {
+
+        player = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
