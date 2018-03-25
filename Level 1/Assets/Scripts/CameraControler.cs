e92c88ba17ff6088c84755ad3eac17df084f6932
Narith Seang <nseang2@gmail.com>
2018-03-25 00:09:25 -0400

    added jump animation and rubberband cam

---
+++ Level 1/Assets/Scripts/CameraControler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float smoothSpeed = 0.25f;
+    [SerializeField]
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        Vector3 desiredposition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredposition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+
+    }
+}
