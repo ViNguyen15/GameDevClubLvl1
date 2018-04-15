2046ff60ed82bdfb4f45594f2ca9cbe4e436a8d2
Narith Seang <nseang2@gmail.com>
2018-04-14 23:18:45 -0400

    enemy can now shoot at the player

--- Level 1/Assets/Scripts/ShootBugController.cs
+++ Level 1/Assets/Scripts/ShootBugController.cs
@@ -11,26 +11,48 @@ public class ShootBugController : MonoBehaviour {
     [SerializeField]
     private GameObject firePoint;
 
-	// Use this for initialization
-	void Start () {
+    private GameObject cObject;
+
+    //health
+    private float startingHealth = 15f;
+    private float currentHealth;
+
+    // Use this for initialization
+    void Start () {
+
+        currentHealth = startingHealth;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-      // ShootAtPlayer();
-	}
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Player")
-        {
-            ShootAtPlayer();
-            Debug.Log("Player Targeted");
-        }
     }
 
-    private void ShootAtPlayer()
+    //taking damage
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "PBullet")
+        {
+            cObject = collision.gameObject;
+            float dmg = cObject.GetComponent<DamageController>().getDmg();
+
+            if (cObject != null)
+            {
+                currentHealth -= dmg;
+
+            }
+
+            if (currentHealth <= 0)
+            {
+                //isDead = true;
+                Destroy(gameObject);
+            }
+        }
+
+    }
+
+    public void ShootAtPlayer()
     {
         Rigidbody2D eBulletClone = Instantiate(eBullet, firePoint.transform.position, transform.rotation);
         eBulletClone.velocity = new Vector2(bulletSpeed, 0);
