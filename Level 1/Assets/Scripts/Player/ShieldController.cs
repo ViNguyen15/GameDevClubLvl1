45c726043ba5744c20365f72ddfa4e44f79f8b34
Vi Nguyen <fluffywin@gmail.com>
2018-05-11 18:54:17 -0400

    added Shield

---
+++ Level 1/Assets/Scripts/Player/ShieldController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldController : MonoBehaviour {
+
+    private float timer = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Dissapate();
+	}
+
+    private void Dissapate()
+    {
+        timer -= Time.deltaTime;
+        if(timer < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
